964dcd42d56785fe84458aa5df98352ab3cc7cb9
Yucheng Liu <liuyuc1994@gmail.com>
2018-10-07 22:16:01 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyDuu6B1c8UMloYs7Ef0LlJQ4Iy8zdrM06M',
-    authDomain: 'chatroom-1203a.firebaseapp.com',
-    databaseURL: 'https://chatroom-1203a.firebaseio.com',
-    projectId: 'chatroom-1203a',
-    storageBucket: 'chatroom-1203a.appspot.com',
-    messagingSenderId: '779050122214'
-  }
 };
 
 /*
